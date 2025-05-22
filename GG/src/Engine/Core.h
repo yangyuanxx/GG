0c099be195b0d18dff7ce6175c4e27124206b1d6
踏影 <taying.yy@alibaba-inc.com>
2025-05-22 18:54:34 +0800

    chore: modify

--- GG/src/Engine/Core.h
+++ GG/src/Engine/Core.h
@@ -7,6 +7,7 @@
 #define GG_ASSERT(x, ...) { if(!(x)) { GG_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
 #define GG_CORE_ASSERT(x, ...) { if(!(x)) { GG_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); } }
 
+// 生成只有第 x 位为 1 的掩码
 #define BIT(x) (1 << x)
 
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -1,38 +1,51 @@
 #include <GG.h>
 #include "imgui.h"
 
-class ExampleLayer : public GG::Layer {
-  public:
-    ExampleLayer(): Layer("Example") {
-    }
+class ExampleLayer : public GG::Layer
+{
+public:
+  ExampleLayer() : Layer("Example")
+  {
+  }
 
-    void OnUpdate() override {
-      if (GG::Input::IsKeyPressed(GG_KEY_TAB)) {
-        GG_INFO("Tab key is pressed!");
-      }
+  void OnUpdate() override
+  {
+    if (GG::Input::IsKeyPressed(GG_KEY_TAB))
+    {
+      GG_INFO("Tab key is pressed!");
     }
+  }
 
-    void OnEvent(GG::Event& event) override {
-      // GG_TRACE("{0}", event.ToString());
-    }
+  void OnEvent(GG::Event &event) override
+  {
+    // GG_TRACE("{0}", event.ToString());
+  }
 
-    virtual void OnImGuiRender() override {
-      ImGui::Begin("Hello");
-      ImGui::Text("Hello, world!");
-      ImGui::End();
-    }
+  virtual void OnImGuiRender() override
+  {
+    ImGui::Begin("Hello");
+    ImGui::Text("Hello, world!");
+    ImGui::Button("Button");
+
+    
+    ImGui::End();
+  }
 };
 
-class Sandbox : public GG::Application {
-  public:
-    Sandbox() {
-      PushLayer(new ExampleLayer());
-    }
-    ~Sandbox() {
-      printf("Sandbox end\n");
-    }
+class Sandbox : public GG::Application
+{
+public:
+  Sandbox()
+  {
+    PushLayer(new ExampleLayer());
+  }
+  ~Sandbox()
+  {
+    printf("Sandbox end\n");
+  }
 };
 
-GG::Application* GG::CreateApplication() {
+GG::Application *GG::CreateApplication()
+{
   return new Sandbox();
 }
